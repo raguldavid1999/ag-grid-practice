b9d86783b78206e0e3998fc63707a2830bfdcc97
raguldavid1999 <raguldavid1999@gmail.com>
2025-05-04 17:34:29 +0530

    change names as userNames

--- src/app/components/open-button/open-button.component.ts
+++ src/app/components/open-button/open-button.component.ts
@@ -10,13 +10,13 @@ import { Router } from '@angular/router';
 })
 export class OpenButtonComponent {
   constructor(private router: Router){}
-  names: WritableSignal<string[]> = signal(['Ragul','David', 'Nivetha', 'Kanmani', 'Robert', 'Perumal']);
+  userNames: WritableSignal<string[]> = signal(['Ragul','David', 'Nivetha', 'Kanmani', 'Robert', 'Perumal']);
   redirectToCountries(){
     this.router.navigate(['/countries']);
   }
 
   changeName(){
     // this.names[4] = 'Rukmani';
-    this.names.set(['Ragul','David', 'Nivetha', 'Kanmani', 'Rukmani', 'Perumal'])
+    this.userNames.set(['Ragul','David', 'Nivetha', 'Kanmani', 'Rukmani', 'Perumal'])
   }
 }
